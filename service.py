1682000c3c2c2d0a98c9508acc63e0fe2c42421a
Ben Salha Mohamed Amine <mohamed-amine.ben-salha@student-cs.fr>
2023-02-23 16:27:34 +0100

    edit

--- service.py
+++ service.py
@@ -1,35 +1,13 @@
-from __future__ import annotations
-
-import typing as t
-from typing import TYPE_CHECKING
-
-import numpy as np
-from PIL.Image import Image as PILImage
-import torch.nn.functional as F
-import torchvision.transforms.functional as TF
-
 import bentoml
-from bentoml.io import Image
 from bentoml.io import NumpyNdarray
 
-if TYPE_CHECKING:
-    from numpy.typing import NDArray
+model_runner = bentoml.sklearn.get("regressor:latest").to_runner()
 
-model_runner = bentoml.pytorch.get("pytorch").to_runner()
+svc = bentoml.Service("regression", runners=[model_runner])
 
-svc = bentoml.Service(name="pytorch_demo", runners=[model_runner])
-
-
-def to_numpy(tensor):
-    return tensor.detach().cpu().numpy()
+input_spec = NumpyNdarray(dtype="int", shape=(-1, 5))
 
 
-@svc.api(input=Image(), output=NumpyNdarray(dtype="int64"))
-async def predict_image(f: PILImage) -> NDArray[t.Any]:
-    assert isinstance(f, PILImage)
-    img = TF.to_tensor(input)
-    img = TF.normalize(img, [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    img = img.unsqueeze(0)
-
-    output_tensor = await model_runner.async_run(img)
-    return to_numpy(output_tensor)
+@svc.api(input=input_spec, output=NumpyNdarray())
+async def predict(input_arr):
+    return await model_runner.predict.async_run(input_arr)
--- train.py
+++ train.py
@@ -1,107 +1,38 @@
-# pylint: disable=redefined-outer-name
-import argparse
-from torch.utils.data import DataLoader, random_split
-import model as models
-import torch
-import torch.nn as nn
-import torch.optim as optim
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import numpy as np
+import pandas as pd
+import boto3
+
 import bentoml
-from tqdm import tqdm
-from utils import DatasetGenerator
 
-ce_loss = nn.CrossEntropyLoss()
-
-
-def test_model(model, test_loader, device="cpu"):
-    correct, num_predictions = 0, 0
-    with tqdm(total=len(test_loader), desc="Test") as pb:
-        model.eval()
-        for batch_num, (img, img_label) in enumerate(test_loader):
-            img = img.to(device)
-            img_label = img_label.to(device)
-            predictions = model(img)
-            correct += (torch.argmax(predictions, dim=1) == img_label).sum().item()
-            num_predictions += predictions.shape[0]
-            pb.update(1)
-    return correct, num_predictions
-
-
-def train(train_loader, device="cpu"):
-
-    class_num = 2
-    # set hyperparameters
-    lr = 0.001
-    weight_decay = 0.0005
-    epoch = 1
-    # initialize models
-    model = models.ResNet(class_num=class_num)
-    model = model.to(device)
-
-    # define optimizers
-    opt_model = optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=weight_decay)
-    # train
-    model.train()
-    torch.set_grad_enabled(True)
-
-    for epo in range(1, epoch+1):
-        correct_resnet = 0
-        print("Epoch {}/{} \n".format(epo, epoch))
-        with tqdm(total=len(train_loader), desc="Train") as pb:
-            for batch_num, (img, img_label) in enumerate(train_loader):
-                opt_model.zero_grad()
-                img = img.to(device)
-                img_label = img_label.to(device)
-                outputs = model(img)
-                correct_resnet += (torch.argmax(outputs, dim=1) == img_label).sum().item()
-                loss = ce_loss(outputs, img_label)
-                loss.backward()
-                opt_model.step()
-                pb.update(1)
-    return model
 
 if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description="BentoML PyTorch")
-    parser.add_argument(
-        "--cuda", action="store_true", default=True, help="enable CUDA training"
-    )
-    parser.add_argument(
-        "--model-name",
-        type=str,
-        default="pytorch",
-        help="name for saved the model",
-    )
-    batch_size = 64
-    data_root = "diffusion/pizza-not-pizza"
-    args = parser.parse_args()
-    use_cuda = args.cuda and torch.cuda.is_available()
-
-    device = torch.device("cuda" if use_cuda else "cpu")
-
-    dataset = DatasetGenerator(data_root)
-    train_set_length = int(0.7 * len(dataset))
-    test_set_length = len(dataset) - train_set_length
-    # split the dataset
-    train_set, test_set = random_split(dataset, [train_set_length, test_set_length])
-    # define loaders
-    train_loader = DataLoader(train_set, shuffle=True, batch_size=batch_size)
-    test_loader = DataLoader(test_set, shuffle=True, batch_size=batch_size)
-
-    trained_model = train(train_loader, device)
-    correct, total = test_model(trained_model, test_loader, device)
-
-    # training related
+    # prepare data
+    s3 = boto3.client('s3',
+        endpoint_url='https://minio.lab.sspcloud.fr/',
+                     )
+    data = s3.get_object(Bucket="mbenxsalha", Key="diffusion/yatai_data.csv")["Body"]
+    df = pd.read_csv(data)
+    features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+    x, y = df[features], df['Price']
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, shuffle=True)
+    
+    # train model
+    model = RandomForestRegressor(max_depth=8, random_state=0)
+    model.fit(x_train, y_train)
+    
+    # evaluate model
+    y_pred = model.predict(x_test)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    
+    # save model
     metadata = {
-        "acc": float(correct) / total
+        "RMSE": rmse
     }
-
-    signatures = {"predict": {"batchable": True}}
-
-    saved_model = bentoml.pytorch.save_model(
-        args.model_name,
-        trained_model,
-        signatures=signatures,
-        metadata=metadata,
-        external_modules=[models],
-    )
-    print(f"Saved model: {saved_model}")
+    bento_model = bentoml.sklearn.save_model(
+        "regressor",
+        model, 
+        metadata=metadata)
+    print(f"Model saved: {bento_model}")
